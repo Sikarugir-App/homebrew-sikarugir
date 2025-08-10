42b6308d133a6a2f2119892c9d42e640f3adaa04
Dean M Greer <38226388+Gcenx@users.noreply.github.com>
2025-08-09 22:08:57 -0400

    Update sikarugir.rb

--- Casks/sikarugir.rb
+++ Casks/sikarugir.rb
@@ -1,25 +1,23 @@
 cask "sikarugir" do
-  version "2.0.4"
+  version "1.0"
   sha256 "626f9bf9e6c29493906e371a6f1eb72acecb21cef68516890eb98ef59e844160"
 
-  url "https://github.com/Sikarugir-App/Winery/releases/download/v#{version}/winery-v#{version}.tar.xz"
-  name "Winery"
+  url "https://github.com/Sikarugir-App/Creator/releases/download/v#{version}/creator-v#{version}.tar.xz"
+  name "Sikarugir Creator"
   desc "Porting tool, to make Windows programs/games into native apps"
   homepage "https://github.com/Sikarugir-App"
 
   depends_on macos: ">= :catalina"
 
-  app "Winery.app", target: "Sikarugir Winery.app"
+  app "Creator.app", target: "Sikarugir Creator.app"
 
   postflight do
-    system "/usr/bin/xattr", "-drs", "com.apple.quarantine", "#{appdir}/Sikarugir Winery.app"
-    system "/usr/bin/codesign", "--force", "--deep", "-s", "-", "#{appdir}/Sikarugir Winery.app"
+    system "/usr/bin/xattr", "-drs", "com.apple.quarantine", "#{appdir}/Sikarugir Creator.app"
+    system "/usr/bin/codesign", "--force", "--deep", "-s", "-", "#{appdir}/Sikarugir Creator.app"
   end
 
   zap trash: [
-    "~/Library/Application Support/Kegworks",
-    "~/Library/Caches/com.unofficial.winery",
-    "~/Library/Caches/com.unofficial.wineskin",
+    "~/Library/Application Support/Sikarugir",
   ]
 
   caveats do
