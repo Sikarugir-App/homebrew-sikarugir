f40f3149ea598ec4fedd052c8784dc1b3b0a2694
Dean M Greer <38226388+Gcenx@users.noreply.github.com>
2025-08-13 20:52:49 -0400

    Update sikarugir.rb

--- Casks/sikarugir.rb
+++ Casks/sikarugir.rb
@@ -2,7 +2,7 @@ cask "sikarugir" do
   version "1.0"
   sha256 "f98d737bf4d1b5274e4a74102b8995ab57fcbb408343cb5d631a09cc4d96cb0f"
 
-  url "https://github.com/Sikarugir-App/Creator/releases/download/v#{version}/creator-v#{version}.tar.xz"
+  url "https://github.com/Sikarugir-App/Creator/releases/download/v#{version}/Creator-v#{version}.tar.xz"
   name "Sikarugir Creator"
   desc "Porting tool, to make Windows programs/games into native apps"
   homepage "https://github.com/Sikarugir-App"
